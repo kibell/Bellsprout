2e16887a1f696f3ed6f001a45524d3807e532c87
Kitrick Bell <kitrickd.bell@gmail.com>
2020-07-08 18:05:15 -0500

    adding new sections to the website

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+"use strict";
+$('#slider-carousel').carouFredSel({
+    responsive:true,
+    width:'100%',
+    circular:true,
+    scroll:{
+            items:1,
+            duration:500,
+            pauseOnHover:true
+    },
+    auto:true,
+    items:{
+        visible:{
+            min:1,
+            max:1
+        },
+    height:"variable"
+    },
+    pagination:{
+        container:".sliderpager",
+        pageAnchorBuilder:false
+    }
+
+});
+
+
+$(window).scroll(function(){
+const top = $(window).scrollTop();
+if(top >=75){
+    $("header").addClass('secondary');
+}
+else if($("header").hasClass('secondary')){
+    $("header").removeClass('secondary')
+}
+
+})
+
+})
