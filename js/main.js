b3ef93f9fbcd7aad2b5c37fd8a13e68f4c758304
Kitrick Bell <kitrickd.bell@gmail.com>
2020-07-12 20:05:18 -0500

    adding new sections to the website and styling some basic components

--- js/main.js
+++ js/main.js
@@ -1,29 +1,7 @@
 $(document).ready(function(){
 
 "use strict";
-$('#slider-carousel').carouFredSel({
-    responsive:true,
-    width:'100%',
-    circular:true,
-    scroll:{
-            items:1,
-            duration:500,
-            pauseOnHover:true
-    },
-    auto:true,
-    items:{
-        visible:{
-            min:1,
-            max:1
-        },
-    height:"variable"
-    },
-    pagination:{
-        container:".sliderpager",
-        pageAnchorBuilder:false
-    }
 
-});
 
 
 $(window).scroll(function(){
