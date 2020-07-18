6884386925f5886e77e64d8a95bae4c9a09bc1a2
Kitrick Bell <kitrickd.bell@gmail.com>
2020-07-18 09:24:03 -0500

    created main website design and added animations

--- js/main.js
+++ js/main.js
@@ -1,23 +1,63 @@
-$(document).ready(function(){
 
 "use strict";
 
 
 
-$(window).scroll(function(){
-const top = $(window).scrollTop();
-if(top >=75){
-    $("header").addClass('secondary');
-}
-else if($("header").hasClass('secondary')){
-    $("header").removeClass('secondary')
-}
+// $(window).scroll(function(){
+// const top = $(window).scrollTop();
+// if(top >=75){
+//     $("header").addClass('secondary');
+// }
+// else if($("header").hasClass('secondary')){
+//     $("header").removeClass('secondary')
+// }
+
+// })
+
+
+// })
+
+const selectElement = function (element) {
+    return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle')
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function(){
+    body.classList.toggle('open');
+})
+
+//scroll reveal
+
+window.sr =  ScrollReveal();
+
+sr.reveal('.animate-left', {
+
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
 
 })
 
- 
 
 
+sr.reveal('.animate-top', {
 
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
 
 })
+
+sr.reveal('.animate-bottom', {
+
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+
+})
+
